10c9bdefe65ecf59e864f7e5fc05e91dc597087e
er21jq <rene.van.bpi.putten@ing.com>
2020-03-17 08:52:17 +0100

    Changed FixedGrid into mutable grid (content mutable, not size mutable)

--- src/main/java/com/putoet/utils/FixedGrid.java
+++ src/main/java/com/putoet/utils/FixedGrid.java
@@ -2,6 +2,7 @@ package com.putoet.utils;
 
 public interface FixedGrid<T> {
     T get(int x, int y);
+    void set(int x, int y, T value);
 
     int height();
 
--- src/main/java/com/putoet/utils/FixedNonNegativeGrid.java
+++ src/main/java/com/putoet/utils/FixedNonNegativeGrid.java
@@ -5,10 +5,8 @@ import java.util.List;
 
 public class FixedNonNegativeGrid<T> implements FixedGrid<T> {
     private final List<List<T>> grid;
-    private final T defaultValue;
     private final int height;
     private final int width;
-    private final boolean hasDefaultValue;
 
     public FixedNonNegativeGrid(int height, int width, T defaultValue) {
         assert height > 0;
@@ -17,8 +15,6 @@ public class FixedNonNegativeGrid<T> implements FixedGrid<T> {
 
         this.height = height;
         this.width = width;
-        this.defaultValue = defaultValue;
-        this.hasDefaultValue = true;
         this.grid = new ArrayList<>();
 
         for (int idy = 0; idy < height; idy++) {
@@ -36,18 +32,26 @@ public class FixedNonNegativeGrid<T> implements FixedGrid<T> {
 
         this.height = matrix.size();
         this.width = matrix.get(0).size();
-        this.defaultValue = null;
-        this.hasDefaultValue = false;
 
         for (List<T> ts : matrix) assert ts.size() == this.width;
 
-        this.grid = new ArrayList<>(matrix);
+        this.grid = new ArrayList<>();
+        for (int idy = 0; idy < height; idy++)
+            this.grid.add(new ArrayList<>(matrix.get(idy)));
     }
 
     @Override
     public T get(int x, int y) {
         assertCoordinates(x, y);
+
         return grid.get(y).get(x);
+    }
+
+    @Override
+    public void set(int x, int y, T value) {
+        assertCoordinates(x, y);
+
+        grid.get(y).set(x, value);
     }
 
     @Override
