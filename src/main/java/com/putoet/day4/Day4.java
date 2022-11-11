08e2ac269b70352b9dbcaa2fc20966addc48370e
Rene van Putten <z669016@gmail.com>
2022-11-11 08:04:58 +0100

    did some refactoring

--- src/main/java/com/putoet/day4/Day4.java
+++ src/main/java/com/putoet/day4/Day4.java
@@ -6,23 +6,22 @@ import java.util.Optional;
 
 public class Day4 {
     public static void main(String[] args) {
-        final var encryptedNames= ResourceLines.stream("/day4.txt")
+        final var encryptedNames = ResourceLines.stream("/day4.txt")
                 .map(EncryptedName::from)
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .toList();
-        final var validNames = encryptedNames.size();
 
-        System.out.println("Number of valid rooms is: " + validNames);
+        System.out.println("Number of valid rooms is: " + encryptedNames.size());
         System.out.println("Sum of the sector id's is: " + encryptedNames.stream()
                 .mapToInt(EncryptedName::sectorId)
                 .sum());
 
-        encryptedNames.forEach(encryptedName -> {
-                    final String decryptedName = encryptedName.decrypt();
-                    if (decryptedName.equals("northpole object storage"))
-                        System.out.println("Sector id for North Pole object storage is: " + encryptedName.sectorId());
-                }
+        System.out.println("Sector id for North Pole object storage is: " + encryptedNames.stream()
+                .filter(name -> name.decrypt().contains("northpole"))
+                .findFirst()
+                .orElseThrow()
+                .sectorId()
         );
     }
 }
