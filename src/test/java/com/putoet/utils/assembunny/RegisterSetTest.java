d1008b57ee4b13376ebab1512cc958ab7311dd44
Rene van Putten <z669016@gmail.com>
2023-08-27 13:55:01 +0200

    Introduced Timer, and var, reduced scope where possible, minor enhancement

--- src/test/java/com/putoet/utils/assembunny/RegisterSetTest.java
+++ src/test/java/com/putoet/utils/assembunny/RegisterSetTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
