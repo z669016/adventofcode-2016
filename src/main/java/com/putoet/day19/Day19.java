1231b6437eec84e51f9381f5202027a7cfa6bb3c
Rene van Putten <z669016@gmail.com>
2023-08-28 13:05:57 +0200

    Introduced Timer, and var, reduced scope where possible, minor enhancement

--- src/main/java/com/putoet/day19/Day19.java
+++ src/main/java/com/putoet/day19/Day19.java
@@ -1,21 +1,26 @@
 package com.putoet.day19;
 
 import com.putoet.math.Josephus;
+import com.putoet.utils.Timer;
 
 import java.util.*;
 
 public class Day19 {
     public static void main(String[] args) {
-        final int puzzleInput = 3004953;
-        System.out.println("The winning Elf in a game with " + puzzleInput + " elves is " + Josephus.winner(puzzleInput));
-        System.out.println("The winner using opposite strategy is " + part2(puzzleInput));
+        final var puzzleInput = 3004953;
+
+        Timer.run(() ->
+                System.out.println("The winning Elf in a game with " + puzzleInput + " elves is " + Josephus.winner(puzzleInput))
+        );
+
+        Timer.run(() -> System.out.println("The winner using opposite strategy is " + part2(puzzleInput)));
     }
 
     public static int part2(int teamSize) {
-        final Deque<Integer> left = new LinkedList<>();
-        final Deque<Integer> right = new LinkedList<>();
+        final var left = new LinkedList<Integer>();
+        final var right = new LinkedList<Integer>();
 
-        for (int i = 1; i < teamSize + 1; i++) {
+        for (var i = 1; i < teamSize + 1; i++) {
             if (i < teamSize / 2 + 1)
                 left.offerLast(i);
             else
